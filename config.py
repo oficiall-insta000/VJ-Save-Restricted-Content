0e22016094098c0076821d7c381d0febe4c5b3e4
official <hisokaff88@gmail.com>
2025-04-12 20:33:21 +0100

    config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ADMINS = int(os.environ.get("ADMINS", "5081207021"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://hisokaff88:onQcNCqw0p0VZRsf@cluster0.jhyaujh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
